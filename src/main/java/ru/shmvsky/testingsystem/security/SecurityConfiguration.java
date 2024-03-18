6167f3e36a48f5ae3f37bdddefc4536fe787e891
shmvsky <shmvsky1337@gmail.com>
2024-03-18 22:43:02 +0300

    adds not working templates

--- src/main/java/ru/shmvsky/testingsystem/security/SecurityConfiguration.java
+++ src/main/java/ru/shmvsky/testingsystem/security/SecurityConfiguration.java
@@ -20,7 +20,7 @@ public class SecurityConfiguration {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/login", "/registration").permitAll()
+                .requestMatchers("/login", "/register").permitAll()
                 .requestMatchers("/my-tests/**").hasRole("TEACHER")
                 .anyRequest().authenticated());
 
--- src/main/resources/db/migration/V2__populate_db.sql
+++ src/main/resources/db/migration/V2__populate_db.sql
@@ -1,2 +1,11 @@
 INSERT INTO usr (id, fullname, username, password, role)
-VALUES (nextval('usr_seq'), 'Шумовский Константин Романович', 'shmvsky', '123', 'ROLE_STUDENT');
+VALUES (nextval('usr_seq'), 'Шумовский Константин Романович', 'shmvsky', '111', 'ROLE_TEACHER');
+
+INSERT INTO usr (id, fullname, username, password, role)
+VALUES (nextval('usr_seq'), 'Алферов Василий Михайлович', 'rancor', '222', 'ROLE_STUDENT');
+
+INSERT INTO usr (id, fullname, username, password, role)
+VALUES (nextval('usr_seq'), 'Вязников Илья Алексеевич', 'kadises', '333', 'ROLE_STUDENT');
+
+INSERT INTO usr (id, fullname, username, password, role)
+VALUES (nextval('usr_seq'), 'Иванов Илья Александрович', 'pretty_boy', '444', 'ROLE_STUDENT');
